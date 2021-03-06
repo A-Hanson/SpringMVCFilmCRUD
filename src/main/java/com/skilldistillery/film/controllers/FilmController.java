49a451d98003e5dd5ebd6f804b73137e65ead3ae
Anna Hanson <>
2021-03-06 12:09:50 -0700

    add request mapping methods for creating and updating films

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -2,12 +2,15 @@ package com.skilldistillery.film.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.skilldistillery.film.dao.FilmDAO;
+import com.skilldistillery.film.entities.Film;
 
 @Controller
 public class FilmController {
@@ -39,6 +42,32 @@ public class FilmController {
 		mv.setViewName("WEB-INF/views/FilmList.jsp");
 		mv.addObject("film", filmDAO.findFilmByKeyword(findFilmByKeyword) );
 		return mv;
+	}
+	
+	@RequestMapping(path="NewFilm.do", method= RequestMethod.POST)
+	public ModelAndView createNewFilm(Film film, RedirectAttributes redir) {
+		filmDAO.createFilm(film);
+		ModelAndView mv = new ModelAndView();
+		redir.addFlashAttribute("film", film);
+		mv.setViewName("redirect:filmCreated.do");
+		return mv;
+	}
+	@RequestMapping(path="filmCreated.do", method= RequestMethod.GET)
+	public String newFilm(Model model) {
+		return "WEB-INF/views/Film.jsp";
+	}
+	
+	@RequestMapping(path="UpdateFilm.do", method= RequestMethod.POST)
+	public ModelAndView updateFilm(Film film, RedirectAttributes redir) {
+		filmDAO.createFilm(film);
+		ModelAndView mv = new ModelAndView();
+		redir.addFlashAttribute("film", film);
+		mv.setViewName("redirect:filmUpdated.do");
+		return mv;
+	}
+	@RequestMapping(path="filmUpdated.do", method= RequestMethod.GET)
+	public String refreshFilm(Model model) {
+		return "WEB-INF/views/Film.jsp";
 	}
 	
 }
